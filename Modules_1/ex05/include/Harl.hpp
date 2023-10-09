1f8c789e77a0436b1b526e65d8fe32749d7c806c
Zackary Trottier <ztrottie@c1r3p7.42quebec.com>
2023-10-09 16:53:42 -0400

    yessir

--- Modules_1/ex05/include/Harl.hpp
+++ Modules_1/ex05/include/Harl.hpp
@@ -1,2 +1,15 @@
 #pragma once
+#include <iostream>
 
+class Harl
+{
+private:
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+public:
+	Harl(void);
+	~Harl(void);
+	void	complain(std::string level) const ;
+};
---
+++ Modules_1/ex05/src/Harl.cpp
@@ -0,0 +1,32 @@
+#include "../include/Harl.hpp"
+
+using std::cout;
+using std::endl;
+
+Harl::Harl(void) {
+	return;
+}
+
+Harl::~Harl(void) {
+	return;
+}
+
+void	Harl::debug(void) {
+	cout << "J'aime quand mes spell parte ou qui sont supposer" << endl;
+}
+
+void	Harl::info(void) {
+	cout << "Yasuo est l'un des pire champion de league of legend, hasagi mon cul!" << endl;
+}
+
+void	Harl::warning(void) {
+	cout << "Si je tombe contre un darius 1.3M de point je vais rager pis sa seras pas le fun!" << endl;
+}
+
+void	Harl::error(void) {
+	cout << "Voyon donc qui ma tuer ses ben un jeu de marde" << endl;
+}
+
+void complain(std::string level) {
+	
+}
