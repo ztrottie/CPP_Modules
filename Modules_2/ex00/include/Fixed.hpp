61360283aa294d47abbe8150a06034cb165eec7a
Zackary Trottier <ztrottie@c2r5p7.42quebec.com>
2023-10-11 13:55:39 -0400

    va chier

--- Modules_2/ex00/include/Fixed.hpp
+++ Modules_2/ex00/include/Fixed.hpp
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ztrottie <ztrottie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 12:41:28 by ztrottie          #+#    #+#             */
-/*   Updated: 2023/10/10 12:41:29 by ztrottie         ###   ########.fr       */
+/*   Created: 2023/10/11 09:28:38 by ztrottie          #+#    #+#             */
+/*   Updated: 2023/10/11 09:28:39 by ztrottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <iostream>
-
-
-class Harl
-{
-private:
-	void		debug(void);
-	void		info(void);
-	void		warning(void);
-	void		error(void);
-
-public:
-	Harl(void);
-	~Harl(void);
-	void	complain(std::string level);
-};
--- Modules_2/ex00/src/main.cpp
+++ Modules_2/ex00/src/main.cpp
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ztrottie <ztrottie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 12:41:28 by ztrottie          #+#    #+#             */
-/*   Updated: 2023/10/10 12:41:29 by ztrottie         ###   ########.fr       */
+/*   Created: 2023/10/11 09:28:32 by ztrottie          #+#    #+#             */
+/*   Updated: 2023/10/11 09:28:56 by ztrottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#include <iostream>
-
-
-class Harl
-{
-private:
-	void		debug(void);
-	void		info(void);
-	void		warning(void);
-	void		error(void);
-
-public:
-	Harl(void);
-	~Harl(void);
-	void	complain(std::string level);
-};
+int	main(void) {
+	
+}
