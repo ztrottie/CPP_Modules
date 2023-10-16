73eda931e8bbb2926096b6d875947fa015f6e886
Zackary Trottier <ztrottie@c2r5p7.42quebec.com>
2023-10-16 17:16:11 -0400

    done with ex01

--- Modules_2/ex01/include/Fixed.hpp
+++ Modules_2/ex01/include/Fixed.hpp
@@ -6,22 +6,30 @@
 /*   By: ztrottie <ztrottie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 09:28:38 by ztrottie          #+#    #+#             */
-/*   Updated: 2023/10/16 14:01:44 by ztrottie         ###   ########.fr       */
+/*   Updated: 2023/10/16 16:45:24 by ztrottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 class Fixed
 {
 private:
-	int					value_;
-	static int const	fractionalBits_ = 8;
+	int 				value_;
+	static int const	fractionalBits_;
 
 public:
 	Fixed(void);
 	Fixed(Fixed const &inst);
+	Fixed(int const &nb);
+	Fixed(float const &nb);
 	~Fixed(void);
 
 	Fixed	&operator=(Fixed const &inst);
 	int 	getRawBits(void) const;
 	void	setRawBits(int const raw);
+	float 	toFloat(void) const;
+	int 	toInt(void) const;
 };
+
+std::ostream & operator<<(std::ostream & o, Fixed const & inst);
--- Modules_2/ex01/src/Fixed.cpp
+++ Modules_2/ex01/src/Fixed.cpp
@@ -6,15 +6,19 @@
 /*   By: ztrottie <ztrottie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 09:28:35 by ztrottie          #+#    #+#             */
-/*   Updated: 2023/10/16 14:01:49 by ztrottie         ###   ########.fr       */
+/*   Updated: 2023/10/16 16:56:34 by ztrottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 using std::cout;
 using std::endl;
+using std::roundf;
+
+const int Fixed::fractionalBits_ = 8;
 
 Fixed::Fixed(void) : value_(0){
 	cout << "Default constructor called" << endl;
@@ -27,14 +31,39 @@ Fixed::Fixed(Fixed const &inst) {
 	return;
 }
 
+Fixed::Fixed(int const &nb) {
+	cout << "Int constructor called" << endl;
+	this->value_ = (nb << this->fractionalBits_);
+	return;
+}
+
+Fixed::Fixed(float const &nb) {
+	cout << "Float constructor called" << endl;
+	this->value_ = (roundf(nb * (1 << this->fractionalBits_)));
+	return;
+}
+
 Fixed::~Fixed(void) {
 	cout << "Destructor called" << endl;
 	return;
 }
 
 int Fixed::getRawBits(void) const {
-	cout << "getRawBits member function called" << endl;
+	// cout << "getRawBits member function called" << endl;
 	return this->value_;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	cout << "setRawBits member function called" << endl;
+	this->value_ = raw;
+}
+
+float 	Fixed::toFloat(void) const {
+	return ((float)this->value_ / (float)(1 << this->fractionalBits_));
+}
+
+int 	Fixed::toInt(void) const {
+	return ((int)this->value_ >> this->fractionalBits_);
 }
 
 Fixed &Fixed::operator=(Fixed const &inst) {
@@ -43,7 +72,7 @@ Fixed &Fixed::operator=(Fixed const &inst) {
 	return *this;
 }
 
-void	Fixed::setRawBits(int const raw) {
-	cout << "setRawBits member function called" << endl;
-	this->value_ = raw;
+std::ostream & operator<<(std::ostream & o, Fixed const & inst) {
+	o << inst.toFloat();
+	return o;
 }
